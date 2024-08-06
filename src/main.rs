547dd1bdfdef045bea9965a9a90d6527bddbd93c
Tomoro0726 <tomoro0726@gmail.com>
2024-08-06 20:51:35 +0900

    Cドライブ上のデータだとエラーが発生しない

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use dbase;
 use shapefile::Reader;
-use std::path::Path;
+use std::fs::File;
+use std::io::{self, Read, Seek, SeekFrom};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let shp_path = "data/line/line.shp";
-  let dbf_path = "data/line/line.dbf";
+  let shp_path = "C:/Users/tomor/Downloads/500m_mesh_suikei_2018_shape_13/500m_mesh_2018_13.shp";
+  let dbf_path = "C:/Users/tomor/Downloads/500m_mesh_suikei_2018_shape_13/500m_mesh_2018_13.dbf";
 
-  // ファイルの存在確認
-  if !Path::new(shp_path).exists() {
-    return Err(format!("SHP file does not exist: {}", shp_path).into());
-  }
-  if !Path::new(dbf_path).exists() {
-    return Err(format!("DBF file does not exist: {}", dbf_path).into());
-  }
+  // ファイルの存在確認と読み取り可能性のチェック
+  check_file(shp_path)?;
+  check_file(dbf_path)?;
 
   // SHPリーダーの作成
   let mut shp_reader = Reader::from_path(shp_path)
@@ -34,8 +31,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
       shp_count
     );
   }
-  let shape_type = shp_reader.header().shape_type;
-  println!("Shape type: {:?}", shape_type);
+
+  // シェイプタイプの表示
+  let shape_type = shp_reader.header().shape_type.to_string();
+  println!("Shape type: {}", shape_type);
+
+  // シェイプタイプごとの変換処理
+  if shape_type == "Polygon" {
+    for dbf_record in dbf_reader.iter_records() {
+      match dbf_record {
+        Ok(record) => println!("{:?}", record),
+        Err(e) => println!("Error reading DBF record: {:?}", e),
+      }
+    }
+
+    for shp_record in shp_reader.iter_shapes_and_records() {
+      match shp_record {
+        Ok((shape, record)) => println!("Geometry: {}, Properties {:?}", shape, record),
+        Err(e) => println!("Error reading SHP record: {:?}", e),
+      }
+    }
+  } else {
+    println!("Type:{} is not supported", shape_type);
+  }
+
+  Ok(())
+}
+
+fn check_file(path: &str) -> io::Result<()> {
+  let mut file = File::open(path)?;
+  let mut buffer = [0; 1024];
+
+  // ファイルの先頭を読み取る
+  file.read(&mut buffer)?;
+
+  // ファイルの終わりまでシーク
+  file.seek(SeekFrom::End(0))?;
 
   Ok(())
 }
